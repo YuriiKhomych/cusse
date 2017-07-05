4e1c3e3d2c7bc3de639ef4a171050a26544ff5f0
Yurii Khomych <yuriykhomich@gmail.com>
2017-07-05 21:30:39 +0300

    Done that we do at the lection

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+
 class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
+
+    def __str__(self):
+        return '{} | {}'.format(self.title, self.body)
