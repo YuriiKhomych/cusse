d28b67061ea06a2948d69d65b4d9d2bd11edbeb6
Yurii Khomych <yuriykhomich@gmail.com>
2017-07-09 16:31:28 +0300

    Add code from lecture

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import url
 
-from blog.views import blogs, single_blog
+from blog.views import blogs, single_blog, user_articles
 
 urlpatterns = [
     url(r'^$', blogs, name='all_articles'),
-    url(r'^single/(?P<article_id>\d+)/$', single_blog, name='single_article_page')
+    url(r'^single/(?P<article_id>\d+)/$', single_blog, name='single_article_page'),
+    url(r'^user/(?P<user_id>\d+)/$', user_articles, name='user_articles'),
 ]
 
 
 # /blog/ -> all blogs
-# /blog/single/sdjkghsdjkghdskjgh/ -> single blog
+# /blog/single/12345/ -> single blog
+# /blog/user/123456/ -> single user(with all articles)
