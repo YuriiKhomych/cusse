a03067796fd698a00edad9fd3429c13a988d6abc
Yurii Khomych <yuriykhomich@gmail.com>
2017-07-16 16:01:49 +0300

    Done sign-up form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from accounts.views import sign_in, sign_out
+from accounts.views import sign_in, sign_out, sign_up
 
 urlpatterns = [
     url(r'^sign-in/$', sign_in, name='sign_in'),
     url(r'^sign-out/$', sign_out, name='sign_out'),
+    url(r'^sign-up/$', sign_up, name='sign_up'),
 ]
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, SignUpForm
 
 
 def sign_in(request):
@@ -26,3 +27,21 @@ def sign_in(request):
 def sign_out(request):
     logout(request)
     return HttpResponseRedirect(reverse('sign_in'))
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            data = form.cleaned_data
+            user = authenticate(email=data.get('email'),
+                                password=data.get('password1'))
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(reverse('all_articles'))
+            else:
+                print('sorry')
+    else:
+        form = SignUpForm()
+    return render(request, 'sign-up.html', {'form': form})
