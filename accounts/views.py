0bb8c015d0a72795a045c7fa453b4bd739d8735f
Yurii Khomych <yuriykhomich@gmail.com>
2017-07-30 14:32:58 +0300

    Done fully DRF API HW but need to coding more

--- accounts/views.py
+++ accounts/views.py
@@ -3,22 +3,30 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import JsonResponse
+from django.core.mail import send_mail
+
+from utils import random_word
+from utils import gen_page_list
 
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, ListAPIView
 from rest_framework.authtoken.models import Token
+from rest_framework.pagination import PageNumberPagination
 
 from accounts.forms import LoginForm, SignUpForm
 from accounts.models import User
 
-from accounts.serializers import UserSerializer, UserCreationSerializer, UserChangePasswordSerializer, UserMainInfoSerializer
+from accounts.serializers import UserLoginSerializer, UserSerializer,\
+    UserCreationSerializer, UserChangePasswordSerializer, \
+    UserMainInfoSerializer, UserRegistrationSerializer, \
+    MyUserChangePasswordSerializer, UserForgetPasswordSerializer, \
+    UserShortInfoSerializer
 
 from utils import gen_page_list
-
 
 
 def all_users(request):
@@ -50,7 +58,10 @@ def sign_in(request):
             form = LoginForm(request.POST)
             if form.is_valid():
                 data = form.cleaned_data
-                user = authenticate(email=data.get('email'), password=data.get('password'))
+                user = authenticate(
+                    email=data.get('email'),
+                    password=data.get('password')
+                )
                 if user:
                     login(request, user)
                     return HttpResponseRedirect(reverse('all_articles'))
@@ -115,7 +126,10 @@ def get_user_rest(request):
 @api_view(['POST'])
 def change_user_password(request):
     user = User.objects.get(pk=2)
-    serializer = UserChangePasswordSerializer(data=request.POST, context={'user': user})
+    serializer = UserChangePasswordSerializer(
+        data=request.POST,
+        context={'user': user}
+    )
     if serializer.is_valid():
         validated_data = serializer.validated_data
         user.set_password(validated_data.get('new_password'))
@@ -125,12 +139,163 @@ def change_user_password(request):
         return Response(serializer.errors)
 
 
-class ChangeUserPasswordView(APIView):
+class UserLoginAPI(APIView):
+    """
+    This view will check input user email and password then,
+     if data is valid, login him in system
+    end send back response with user basic information:
+     username, email, first_name, date_joined.
+    """
+    serializer_class = UserLoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if request.user.is_authenticated():
+            return Response({'Sorry, you are already login'},
+                            status=status.HTTP_409_CONFLICT)
+
+        else:
+            if serializer.is_valid():
+                validate_data = serializer.validated_data
+                # authenticate user
+                user = authenticate(email=validate_data.get('email'),
+                                    password=validate_data.get('password'))
+                if user:
+                    # collect information about user
+                    user_data = User.objects.get(
+                        email__iexact=validate_data.get('email'))
+                    context = {
+                        'username': user_data.username,
+                        'email': user_data.email,
+                        'first_name': user_data.first_name,
+                        'date_joined': user_data.date_joined,
+                        }
+                    return Response(context)
+                else:
+                    return 'User does not exist'
+            else:
+                return Response(
+                    serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+
+class UserRegistrationAPI(APIView):
+    """
+    This view will check input user name, email,
+    password then, if data is valid, create new user, login him in system
+    and send back response with user Token.
+    """
+    serializer_class = UserRegistrationSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        # Add permission class in future
+        if request.user.is_authenticated():
+            return Response({'Sorry, you are registered'},
+                            status=status.HTTP_409_CONFLICT)
+        else:
+            if serializer.is_valid():
+                validate_data = serializer.validated_data
+                # create user
+                user = User.objects.create_user(
+                    username=validate_data.get('username'),
+                    email=validate_data.get('email'),
+                )
+                user.set_password(validate_data.get('password'))
+                user.save()
+                # login user
+                login(request, authenticate(
+                    email=validate_data.get('email'),
+                    password=validate_data.get('password')))
+                # create token
+                token = Token.objects.create(user=user)
+                return JsonResponse(token.key)
+            else:
+                return Response(
+                    serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserForgetPasswordAPI(APIView):
+    """
+    This view will check input user name or email,
+    then generate new password and sent it to user email.
+    """
+    serializer_class = UserForgetPasswordSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            validate_data = serializer.validated_data
+            # find user object
+            if '@' in validate_data.get('email_or_username'):
+                user = User.objects.get(
+                    email=validate_data.get('email_or_username'))
+            else:
+                user = User.objects.get(
+                    username=validate_data.get('email_or_username'))
+            # generate and password
+            password = random_word(10)
+            user.set_password(password)
+            user.save()
+            # send mail
+            send_mail('Your new password',
+                      'Hi, it\'s you new password: {}'.format(password),
+                      'yuriykhomich@gmail.com',
+                      [user.email]
+                      )
+            return Response({'success': True})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserShortInfoAPI(ListAPIView):
+    """
+    This view will return response with
+    short information(id, username, email, date_joined) about all users,
+    and divided into 10 users objects at the one page.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserShortInfoSerializer
+    pagination_class = PageNumberPagination
+
+
+class UserChangePasswordAPI(APIView):
+    """
+    This view will check input user old password and new password,
+     then, if data is valid, update user password at base.
+    """
     serializer_class = UserChangePasswordSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def post(self, request):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={'user': request.user})
+
+        if serializer.is_valid():
+            validate_data = serializer.validated_data
+            # create new user password and save it in base
+            request.user.set_password(validate_data.get('new_password1'))
+            request.user.save()
+            return Response({'success': True})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChangeUserPasswordView(APIView):
+    serializer_class = MyUserChangePasswordSerializer
     permission_classes = (IsAuthenticated,)
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data, context={'user': request.user})
+        serializer = self.serializer_class(
+            data=request.data,
+            context={'user': request.user})
         if serializer.is_valid():
             validated_data = serializer.validated_data
             request.user.set_password(validated_data.get('new_password'))
@@ -192,11 +357,9 @@ class UsersView(GenericAPIView):
 class UserCreateView(APIView):
     model = User
     serializer_class = UserCreationSerializer
-
     # token = Token.objects.create(user=User.objects.get(pk=3))
     # queryset = User.objects.all()
     # serializer_class = UserCreationSerializer
-    #
     # def create(self, request, *args, **kwargs):
     #     serializer = self.get_serializer(data=request.data)
     #     serializer.is_valid(raise_exception=True)
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render, HttpResponse, HttpResponseRedirect, Http404, get_object_or_404
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect,\
+    Http404, get_object_or_404
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.core.urlresolvers import reverse
+from django.db.models import Q
 
-from rest_framework.pagination import LimitOffsetPagination, PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.generics import ListAPIView, GenericAPIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.validators import ValidationError
+from rest_framework.permissions import AllowAny
 
 from blog.models import Article
 from blog.forms import SearchForm
-from blog.serializers import ArticleSerializer
+
+from blog.serializers import (
+    ArticleSerializer,
+    ArticleFullDataSerializer,
+    ArticleCreateSerializer,
+    ArticleRemoveSerializer,
+    ArticleSearchSerializer
+)
 
 from utils import gen_page_list, send_email
 
@@ -21,7 +36,8 @@ def blogs(request):
     if form.is_valid():
         # data = form.cleaned_data
         keyword = request.GET.get('search_text', '')
-        articles = Article.objects.filter(title__contains=keyword) # How do it? (title__contains=data.get(keyword))
+        # How do it? (title__contains=data.get(keyword))
+        articles = Article.objects.filter(title__contains=keyword)
     else:
         form = SearchForm()
         articles = Article.objects.all()
@@ -37,9 +53,11 @@ def blogs(request):
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         final_articles = p.page(p.num_pages)
-    return render(request, 'blogs.html', {'articles': final_articles,
-                                          'form': form,
-                                          'pagination': gen_page_list(page, p.num_pages)})
+    return render(request,
+                  'blogs.html',
+                  {'articles': final_articles,
+                   'form': form,
+                   'pagination': gen_page_list(page, p.num_pages)})
 
 
 def single_blog(request, article_id):
@@ -80,7 +98,10 @@ def send_email_rest(request):
         'first_name': 'John',
         'last_name': 'Smith'
     }
-    send_email('hello', 'shovtenko.valya@gmail.com', 'hello-mail.html', content)
+    send_email('hello',
+               'shovtenko.valya@gmail.com',
+               'hello-mail.html',
+               content)
     return Response({'success': True})
 
 
@@ -108,3 +129,93 @@ class ArticlesView(GenericAPIView):
         queryset = self.get_queryset()
         serializer = self.serializer_class(queryset, many=True)
         return Response(serializer.data)
+
+
+class ArticlesAllAPI(ListAPIView):
+    """
+    This view will return response with full articles 
+    (title, body, data, author, liked_by),
+    but divided into 10 users objects at the one response.
+    Used limit and offset.
+    """
+    queryset = Article.objects.all()
+    serializer_class = ArticleFullDataSerializer
+    pagination_class = LimitOffsetPagination
+
+
+class ArticleCreateAPI(APIView):
+    """
+    This view will check input user unique title name and then, 
+    if data is valid, create new article
+    and return success message
+    """
+    serializer_class = ArticleCreateSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            validate_data = serializer.validated_data
+            # create new article and save in base
+            new_article = Article.objects.create(
+                title=validate_data.get('title'),
+                body=validate_data.get('body'),
+                author=request.user
+            )
+            return Response({'success': True})
+        else:
+            return Response(serializer.errors, status=status.HTTP_409_CONFLICT)
+
+
+class ArticleRemoveAPI(APIView):
+    """
+    This view will check if input user title is exist and then, 
+    if data is valid,
+    check if current user is author of article with request title.
+    If check is correct, article with request title will be remove from base.
+    """
+
+    serializer_class = ArticleRemoveSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            validate_data = serializer.validated_data
+            article = Article.objects.get(title=validate_data.get('title'))
+            # check if user is author of article with request title
+            if article.author != request.user:
+                raise ValidationError(
+                    'Sorry, but you can\'t delete not your articles')
+            # remove article with request title
+            else:
+                article.delete()
+                return Response({'success': True})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticlesSearchAPI(APIView):
+    """
+    This view will found all articles by input user keyword in title and body,
+    and return all of them.
+    """
+    serializer_class = ArticleSearchSerializer
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            keyword = serializer.validated_data.get('search_keyword')
+            # filter by input user keyword in articles title and body
+            articles = Article.objects.filter(Q(
+                title__contains=keyword) | Q(body__contains=keyword))
+            return Response(ArticleFullDataSerializer(
+                articles, many=True).data)
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
